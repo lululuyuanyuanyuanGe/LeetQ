631dc61f3ac0e457e5c1d45e9a5c163c7e83af3a
lululuyuanyuanyuanGe <gelululuyuan@gmail.com>
2025-01-07 00:21:46 -0500

    Time: 14 ms (89.67%) | Memory: 13.3 MB (10.43%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -4,18 +4,16 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        string_len = len(s)
-        substring = ''
+        max_len = 0
+        start = 0
+        str_ind = {}
+        
+        for end in range(len(s)):
+            if s[end] in str_ind and str_ind[s[end]] >= start:
+                start = str_ind[s[end]] + 1
 
-        for i in range(string_len):
-            temp_substring = ''
-            for j in range(i, string_len):
-                if s[j] not in temp_substring:
-                    temp_substring += s[j]
-                else:
-                    break
-            if len(temp_substring) > len(substring):
-                substring = temp_substring
-            if len(substring) >= string_len - i - 1:
-                return len(substring)
-        return len(substring)
+            str_ind[s[end]] = end
+
+            max_len = max(max_len, end - start + 1)
+        
+        return max_len
