fc8fa847fd0a9d2f42ce67cc3fa11bb246f68511
lululuyuanyuanyuanGe <gelululuyuan@gmail.com>
2025-01-09 11:32:11 -0500

    Time: 8 ms (31.01%) | Memory: 18.1 MB (9.24%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.py
+++ 12-integer-to-roman/integer-to-roman.py
@@ -1,20 +1,11 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
         roman_rep = ''
-        roman_to_dec = [(1, 'I'),
-                        (4, 'IV'),
-                        (5, 'V'),
-                        (9, 'IX'),
-                        (10, 'X'),
-                        (40, 'XL'),
-                        (50, 'L'),
-                        (90, 'XC'),
-                        (100, 'C'),
-                        (400, 'CD'),
-                        (500, 'D'),
-                        (900, 'CM'),
-                        (1000, 'M')]
-        roman_to_dec.sort(reverse=True)
+        roman_to_dec = [
+            (1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'),
+            (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'), (10, 'X'),
+            (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')
+        ]
         for i in range(len(roman_to_dec)):
             letter_count = num // roman_to_dec[i][0]
             roman_rep += roman_to_dec[i][1] * letter_count
